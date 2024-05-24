a4655edc668689bff0f8dba4bd62392efb4f628a
sarareg0412 <48202466+sarareg0412@users.noreply.github.com>
2024-05-24 17:47:38 +0200

    Time: 6 ms (35.03%) | Memory: 44.8 MB (57.94%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null){
+            if((root.val == p.val || root.val == q.val) ||
+                (root.val > p.val && root.val < q.val) ||
+                (root.val < p.val && root.val > q.val)
+               ){
+                return root;
+            }
+            if(p.val < root.val){
+                root = root.left;
+            }else{
+                root = root.right;
+            }
+            
+             System.out.println(root.val);
+        }
+
+        return null;
+    }
+}

5ab031c6dc4150afd631796c4939c7c2f56f1caf
sarareg0412 <48202466+sarareg0412@users.noreply.github.com>
2024-05-26 19:08:53 +0200

    Time: 21 ms (8.68%) | Memory: 45.7 MB (5.11%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         char[] ransomArray = ransomNote.toCharArray();
-        char[] magazineArray = magazine.toCharArray();
 
         for(char c : ransomArray){
-            int index = new String(magazineArray).indexOf(c);
+            int index = magazine.indexOf(c);
 
             if(index == -1){
                 return false;
             }
 
-            magazineArray[index] = '.';
+            magazine = magazine.replaceFirst("" + c, "");
         }
 
         return true;

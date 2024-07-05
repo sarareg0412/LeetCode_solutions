8fd6bafc6de8073951c99ac329f9079fbdc66586
sarareg0412 <48202466+sarareg0412@users.noreply.github.com>
2024-07-05 16:34:29 +0200

    Time: 1033 ms (24.23%) | Memory: 20.7 MB (37.92%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,28 +4,28 @@ class Solution:
         triples = []
 
         for i in range(len(nums)-2):
-            if nums[i] > 0:
+            if nums[i]>0:
                 break
-            if i> 0 and nums[i] == nums[i-1]:
+            
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
+            
             l = i+1
-            r = len(nums)-1
-            while l < r:
-                tot = nums[i] + nums[l] + nums[r]
-                
-                if tot < 0:
-                    l += 1
-                elif tot > 0:
-                    r -=1 
-                else:
-                    triple = [nums[i], nums[l], nums[r]]
-                    triples.append(triple)
-                    while l < r and nums[l] == triple[1]:
-                        l += 1
-                    while l < r and nums[r] == triple[2]:
-                        r -= 1
+            r = len(nums) - 1
 
-        
+            while l<r:
+                triple = [nums[i], nums[l], nums[r]]
+                if sum(triple) < 0:
+                    l += 1
+                elif sum(triple) > 0:
+                    r -= 1
+                else:
+                    triples.append(triple)
+                    while l<r and nums[l] == triple[1]:
+                            l +=1
+                    while l<r and nums[r] == triple[2]:
+                            r -=1
+
         return triples
+
         
